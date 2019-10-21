9210ebd5c8aaa3a70f2b011596cff44ae620c21f
Gautam <gautam3491@gmail.com>
2019-10-22 12:18:29 +1300

    view estimate for customer

---
+++ showEstimate.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once("headers.php");
+
+$jobs = Estimate::getAllJob($mysqli, $_GET['id']);
+//$jobs->debug();
+echo "<table>";
+foreach($jobs->getRecords() as $id => $job){
+    echo "<tr>";
+    echo "<td>".$job->getJobId()."</td>";
+    echo "<td>".$job->getTradesmanId()."</td>";
+    echo "<td>".$job->getExpirationDate()."</td>";
+    echo "<td>".$job->getEstimateId()."</td>";
+    echo "<td>".$job->getTotalCost()."</td>";
+    echo "<td>".$job->getMaterialCost()."</td>";
+    echo "<td>".$job->getLabourCost()."</td>";
+    // echo "<td><a href=\"showEstimate.php?id=".$job->getId()."\">View</a></td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+
+require_once("footer.php");
+
+?>
+
