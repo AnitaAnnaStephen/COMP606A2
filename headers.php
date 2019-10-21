53f1b264834b4ebc3c95b9f5aa86846300f6e219
Gautam <gautam3491@gmail.com>
2019-10-22 12:46:33 +1300

    change in design

--- headers.php
+++ headers.php
@@ -12,15 +12,18 @@ session_start();
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body style="height:1500px">
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-  <a class="navbar-brand" href="#">Logo</a>
+<!-- <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <a class="navbar-brand" href="#"><img src="logo.jpg" alt="logo" style="width:40px;"></a>
   <ul class="navbar-nav">
     <li class="nav-item">
       <a class="nav-link" href="userPage.php">View Jobs</a>
@@ -55,6 +58,36 @@ session_start();
       </div>
     </li>
   </ul>
-</nav>
 
+</nav> -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    <a class="" href="#"><img src="logo.jpg" alt="logo" style="width:40px;margin-top:10px"></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a class="nav-link" href="userPage.php">View Jobs</a></li>
+      <li><?php 
+                    //toggle to show login, signup and logout button
+                    if($_SESSION['uid']<>''){ 
+                    
+                    echo "<a class=\"nav-link\" href=\"postJob.php\">Post Job</a>";
+                    
+                 }else{
+                       
+                 } 
+                 ?></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+      
+    </ul>
+  </div>
+</nav>
 <div class="container" style="margin-top:80px">
