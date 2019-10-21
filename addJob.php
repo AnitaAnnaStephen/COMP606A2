5f3ccea4c76cead6933853028ff64b34de027af5
student <student@internal.rexnet.nz>
2019-10-22 09:50:19 +1300

    new page added for posting job

---
+++ addJob.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("headers.php");
+
+$job = Job::create($mysqli,$_SESSION['uid'], $_POST['jtype'],$_POST['jdescription'], $_POST['jlocation'], $_POST['crange'], $_POST['actdate'], $_POST['estenddate'],'0');
+
+if (is_null($job)){
+    "<h2>failed to add result</h2>";
+} else {
+    echo "<p>Job Posted </p>";
+   
+    //echo "<p><a href=\"showallStudents.php\">show all students</a></p>";
+}
+
+
+require_once("footer.php");
+
+?>
