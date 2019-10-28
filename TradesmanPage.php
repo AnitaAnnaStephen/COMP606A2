831341fc465161060dc59704fb60d98067eea2ae
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-10-29 12:57:26 +1300

    Updated design

--- TradesmanPage.php
+++ TradesmanPage.php
@@ -33,22 +33,10 @@ require_once("headers.php");
 // }
 // echo "</table>";
 ?>
- <!DOCTYPE html>  
- <html> 
-<body style="background-color: silver;">  
-           <br /><br />  
-           <div class="container" style="width:700px;">  
-           <title>Tradesman Page</title>
-                <h3 align="center"><b>View your Estimates</b></h3>  
-                <br />  
-                <div class="table-responsive">  
-                     <div align="right">  
-                      <!-- <a href="viewJobs.php?tid=<?php echo $_GET['tid']; ?>" class="btn" style="background-color:#fff;">View Jobs</a>   -->
-                     </div>  
-                     <br />  
-                     <div id="bookings_table">  
-                          <table class="table table-bordered">  
-                                <?php  $estimates=Estimate ::findByTradesman($mysqli,$_GET['tid']);
+
+<div class="container" >
+<div class="row">
+<?php  $estimates=Estimate ::findByTradesman($mysqli,$_GET['tid']);
                                 $count=0;
                                 //echo $count;
                                 foreach($estimates->getRecords() as $id => $estimate)
@@ -65,71 +53,46 @@ require_once("headers.php");
                                 }
                                 else
                                 {
-                                    
-                                  echo "<tr>";
-                                  echo "<td><b>Estimate Id</b></td>";
-                                  echo "<td><b>Job Id</b></td>";
-                                  echo "<td><b>Labour Cost</b></td>";
-                                  echo "<td><b>Material Cost</b></td>";
-                                  echo "<td><b>Total Cost</b></td>";
-                                  echo "<td><b>Expiration Date</b></td>";
                                   
-                                  echo "</tr>";
-                                  foreach($estimates->getRecords() as $id => $estimate)
-                                  {
-                                    echo "<tr>";
-                                    echo "<td>".$estimate->getEstimateId()."</td>";
-                                    echo "<td>".$estimate->getJobId()."</td>";
-                                    echo "<td>$ ".$estimate->getLabourCost()."</td>";
-                                    echo "<td>$ ".$estimate->getMaterialCost()."</td>";
-                                    echo "<td>$ ".$estimate->getTotalCost()."</td>";
-                                    echo "<td>".$estimate->getExpirationDate()."</td>";
-                                    echo "<td><a href=\"editEstimate.php?id=". $estimate->getEstimateId()."\"     class=\"btn\">Edit</td>";
-                                 // echo "<td><a href=\"deleteEstimate.php?id=". $estimate->getEstimateId()."\"     class=\"btn\">Delete</td>";
-                                    echo "<td><a href=\"deleteEstimateDB.php?id=".$estimate->getEstimateId()."\" class=\"btn\" onclick=\"return confirm('Are you sure to cancel your estimate?')\">Cancel</td>";
-                                    echo "</tr>";
-                                  }
-                                }
-                               ?>  
-                              
-                          </table>  
-                     </div>  
-                </div>  
-           </div>  
-          
-      </body>
-    </html>
-<!-- Modal -->
-<div id="myPostModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+                                   foreach($estimates->getRecords() as $id => $estimate)
+                                   {?>
+                                   <div class="col-sm-4" >
+      <div class="panel panel-primary">
+      <?php $job= Job :: find($mysqli,$estimate->getJobId());?>
+        <div class="panel-heading"><?php echo $job->getJobType(); ?></div>
+        <div class="panel-body" style="height:400px">
+        <p><b>Job Details</b></p>
+        <p><?php echo $job->getJobDescription(); ?></p>
+        <p>Job Id: <b><?php echo $estimate->getJobId(); ?></b></p>
+        <p>Cost Range: <b><?php echo $job->getCost(); ?></b></p>
+        <p>Estimate Date: <b><?php echo $job->getEstimateDate(); ?></b></p>
+        <p>Active Date: <b><?php echo $job->getActiveDate(); ?></b></p>
+        <p><b>Estimate Details</b></p>
+        <p>Labour Cost: <b><?php echo $estimate->getLabourCost(); ?></b></p>
+        <p>Material Cost: <b><?php echo $estimate->getMaterialCost(); ?></b></p>
+        <p>Total Cost: <b><?php echo $estimate->getTotalCost(); ?></b></p>
+        <p>Date: <b><?php echo $estimate->getExpirationDate(); ?></b></p>
+        
+        </div>
+        <div class="panel-footer pviewestimate" style="text-align: center;">
+            
+            <input type="hidden" value="<?php echo $estimate->getEstimateId(); ?>" >
+            
+            <?php 
+            echo "<a href=\"editEstimate.php?id=".$estimate->getEstimateId()."\"  class=\"btn btn-primary viewestimate\">Edit Estimate</a>";
+            echo "<a href=\"deleteEstimateDB.php?id=".$estimate->getEstimateId()."\" class=\"btn btn-primary viewestimate\" onclick=\"return confirm('Are you sure to cancel your estimate?')\">Cancel Estimate</a>";
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header"  style="text-align:center;background-color:#337ab7;">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Edit Estimate</h2>
-      </div>
-      <div class="modal-body" style="text-align:center;background-color:white;">
-        <p>Enter Estimate Details</p>
-        <form method="post" action="addJobEstimate.php" enctype="application/x-www-form-urlencoded">
- <!-- <p><label>Job Id</label><input name="jobId" type="text" value="<?php echo $job->getJobId();?>"></p>  -->
-<p><label>Job Type</label><input name="jtype" type="text" disabled value="<?php echo $job->getJobType();?>"></p>
-<p><label>Job Description</label><textarea name="jdescription" disabled type="text" value=""><?php echo $job->getJobDescription();?></textarea></p>
-<p><label>Customer Cost Range</label><input name="crange" type="number" disabled value="<?php echo $job->getCost();?>"></p> 
-<p><label>Material Cost</label><input name="mcost" type="number"></p>
-<p><label>Labour Cost</label><input name="lcost" type="number"></p>
-<p><label>Expiration Date</label><input name="expdate" type="date"></p>
+            ?>
+            <!-- <a href="editEstimate.php?id="<?php echo $estimate->getEstimateId(); ?>  class="btn btn-primary viewestimate">Edit Estimate</a>
+            <a href="deleteEstimateDB.php?id="<?php echo $estimate->getEstimateId();?> class="btn btn-primary viewestimate" onclick="return confirm('Are you sure to cancel your estimate?')">Cancel Estimate</a> -->
 
-<input type="hidden" name="tid" value="<?php echo $_GET['tid'];?>">
-<p><input type="submit" value="Post Estimate"></p>
-</form>
+        </div>
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
 </div>
+</div>
+
+<?php }}; ?>
+</div>
+</div>
+ 
 <?php require_once("footer.php"); ?>
