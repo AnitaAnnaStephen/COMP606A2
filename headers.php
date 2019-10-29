4f969daf5ed2063214b40e56da06069fa783ba3d
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-10-29 22:50:50 +1300

    UPdated design

--- headers.php
+++ headers.php
@@ -20,7 +20,7 @@ session_start();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
-<body style="height:1500px;background-color: silver;">
+<body style="height:1500px;background-color: white;">
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
