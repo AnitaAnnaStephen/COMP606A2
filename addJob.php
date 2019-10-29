4a839bd70df08070be9182e0565af23ba0dd9c19
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-10-29 13:56:20 +1300

    Added comments

--- addJob.php
+++ addJob.php
@@ -1,15 +1,19 @@
+<!-- Page to add a new job by the user -->
 <?php
 
 require_once("headers.php");
 
+
+//Calling create function in Job class
 $job = Job::create($mysqli,$_SESSION['uid'], $_POST['jtype'],$_POST['jdescription'], $_POST['jlocation'], $_POST['crange'], $_POST['actdate'], $_POST['estenddate'],'0');
 
 if (is_null($job)){
     "<h2>failed to add result</h2>";
 } else {
     echo "<p>Job Posted </p>";
+    //Redirect to user profile
     header("Location:UserPage.php?uid=".$job->getUserId());
-    //echo "<p><a href=\"showallStudents.php\">show all students</a></p>";
+    
 }
 
 
