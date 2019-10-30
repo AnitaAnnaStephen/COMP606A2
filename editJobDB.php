b4ea89a3465030b13cc3af4a8a4b10992ce28ae8
Gautam <gautam3491@gmail.com>
2019-10-30 13:55:03 +1300

    job post edit classes changes

---
+++ editJobDB.php
@@ -0,0 +1,26 @@
+
+
+<?php 
+require_once("heading.php");
+echo $_POST['jid'];
+echo $_POST['jtype'];
+echo $_POST['jdescription'];
+echo $_POST['location'];
+echo $_POST['sdate'];
+echo $_POST['edate'];
+$editjob=Job :: edit($mysqli,$_POST['jid'], $_POST['jtype'], $_POST['jdescription'], $_POST['location'], $_POST['crange'], $_POST['sdate'], $_POST['edate']);
+ var_dump($editjob);
+if(!$editjob){
+    echo "<h2>Failed to update</h2>";
+}
+ else {
+    echo "<h2>Updated</h2>";
+    $_SESSION['success'] = "Update Successfull..";
+    
+    header("Location: UserPage.php?uid=".$editjob->getUserId());//redirecting to user profile
+    
+}
+
+
+require_once("footer.php");
+?>
