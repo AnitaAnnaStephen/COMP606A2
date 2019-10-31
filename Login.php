eecacccd3dc9bbba2f7fd12b7ab53eaa13656798
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-10-31 14:56:07 +1300

    Updated Design

--- Login.php
+++ Login.php
@@ -8,20 +8,30 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
          
     </head>
-    <body style="background-color:silver;">
+    <body style="background-color:white;">
         <div id="header">
-        <p>
+        <!-- <p>
             <img src="Logo.jpg" alt="Logo" width="100" height="100">
             
                 
             </p>
             <p>
                 <h2>User Login </h2>
-            </p>
+            </p> -->
         </div>
         <div id="login">
             <!-- <form action="" method="POST"> -->
             <form method="post" action="UserCheck.php" enctype="application/x-www-form-urlencoded">
+            <p style="text-align: center;">
+            <img src="Logo.jpg" alt="Logo"  width="150" height="150">
+            
+                
+            </p>
+            <p>
+            </p>
+            <p style="text-align: center;">
+                <h1 style="text-align: center;">User Login </h1>
+            </p>
             <p>
                 <label> UserName</label>    
             </p>
@@ -43,15 +53,24 @@
                     if(isset($_SESSION["error"])){
                         $error = $_SESSION["error"];
                         echo "<span>$error</span>";
+                        $_SESSION["error"]="";
                     }
                 ?>
             </form>
-        </div>
-        <div id="register">
+            <p>
+            
+            </p>
+            <div id="register">
             <p> <label style="font-weight:normal;">Don't have an account? </label>
                 <a href="userRegister.php">Register Now</a>
             </p>
         </div>
+        </div>
+        <!-- <div id="register">
+            <p> <label style="font-weight:normal;">Don't have an account? </label>
+                <a href="userRegister.php">Register Now</a>
+            </p>
+        </div> -->
         <ul style="text-align:center;">
                     
                      <a href="Login.php" style="text-align:right;">Home</a>|
