a2a0d71dd9da903c4eef0842edd919ef97d80919
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-03 10:40:28 +1300

    added comments

--- addTradesman.php
+++ addTradesman.php
@@ -1,7 +1,8 @@
+<!-- Page to add new Tradesman -->
 <?php
 
 require_once("heading.php");
-
+//Calling function to create new tradesman
 $newTradesman = Tradesman::create($mysqli, $_POST['fname'], $_POST['lname'],   $_POST['email'],$_POST['phone'], $_POST['password']);
 if(!$newTradesman){
     echo "<h2>Failed</h2>";
@@ -10,8 +11,6 @@ else {
     echo "<h2>New Tradesman Created</h2>";
     $_SESSION['uid'] = '';
     $_SESSION['tid'] = $newTradesman->getTId();
-    //var_dump ($loggedTradesman);
-    //echo http_build_query($loggedTradesman);
   header("Location: TradesmanPage.php?tid=".$newTradesman->getTId());//redirecting to user profile
 }
 
