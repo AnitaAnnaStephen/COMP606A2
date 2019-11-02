bd73f760d857c6f635d0ce34cff3cc66eb07091d
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-03 01:14:59 +1300

    Design change

--- viewExpiredJobs.php
+++ viewExpiredJobs.php
@@ -29,47 +29,11 @@ require_once("footer.php");
             <input type="hidden" value="<?php echo $job->getJobId(); ?>" >
             <?php $estimate = Estimate::getAcceptedEstimatePerJob($mysqli, $job->getJobId()); ?>
             <a data-toggle="modal" data-target="" class="btn btn-primary viewestimate" name="viewestimate">Rate the Tradesman</a>
-             <!-- Modal -->
-             <div id="" class="modal fade estimate" role="dialog">
-            <div class="modal-dialog modal-lg">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                <div class="modal-header" style="text-align:center;background-color:#337ab7;">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h2 class="modal-title"><b>Rate the Tradesman</b></h2>
-                </div>
-                <div class="modal-body">
-                    
-                    <?php
-                                $tradesman= Tradesman :: findById($mysqli,$estimate->getTradesmanId());
-                                echo "<tr class=".$color.">";
-    
-                                //echo "<td>".$estimate->getJobId()."</td>";
-                                echo "<td>".$estimate->getTradesmanId()."</td>";
-                                echo "<td>".$tradesman->getFName().'  '.$tradesman->getLName()."</td>"; 
-                                
-                                
-                                 echo "<td ".$hid."><a href=\"rateEstimate.php?eid=".$estimate->getEstimateId()."&uid=".$_GET['uid']."\">Accept</a></td>";
-                                echo "</tr>";
-                            }
-                            echo "</table>";
-                      
-                   
-                    ?>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-                </div>
-
-            </div>
-            </div>
-            <!-- end Modal -->
+          
         </div>
       </div>
-    <?php //}
+      </div>
+    <?php }
    ?>
 </div>
 </div>
