2cc8941636ac4329cfa5029fed68198717bbacdd
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-03 10:41:36 +1300

    added comments

--- libs/inc_classes.php
+++ libs/inc_classes.php
@@ -1,3 +1,4 @@
+<!-- PAge to include all classes -->
 <?php
 
 require "classes/Tradesman.php";
@@ -5,5 +6,6 @@ require "classes/Collection.php";
 require "classes/User.php";
 require "classes/Estimate.php";
 require "classes/Job.php";
+require "classes/Review.php";
 
 ?>
