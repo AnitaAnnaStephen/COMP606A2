331c557f11f1e6c676dc0a74f67c90f9bf43d2de
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-03 10:38:15 +1300

    added comments

--- acceptEstimate.php
+++ acceptEstimate.php
@@ -1,17 +1,15 @@
+<!-- PAge that call accept function to accept an estimate -->
 <?php
 
 require_once("heading.php");
-
+//Calling function to accept estimate
 $estimate = Estimate::accept($mysqli, $_GET['eid']);
 if(!$estimate){
     echo "<h2>Failed</h2>";
 }
 else {
     echo "<h2>Accepted</h2>";
-    // $_SESSION['uid'] = $estimate->getUId();
-    // $_SESSION['tid'] = '';
-    //var_dump ($loggedTradesman);
-    //echo http_build_query($loggedTradesman);
+    
   header("Location: userPage.php?uid=".$_GET['uid']);//redirecting to user profile
 }
 
