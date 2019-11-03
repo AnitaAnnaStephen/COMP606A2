561ad9de9a4d23d40aba34ae7427c56ffae039da
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-03 22:55:29 +1300

    Updated design

--- userChangePassword.php
+++ userChangePassword.php
@@ -4,7 +4,7 @@
 require_once("heading.php");
 //Calling function to change Password
 $reset=User :: changePassword($mysqli, $_POST['email'], $_POST['phone'], $_POST['password']);
- var_dump($reset);
+// var_dump($reset);
 if(!$reset){
     $_SESSION['reseterror']="Invalid Email or phone number";
     header("Location: userForgotPassword.php");//redirecting to Forgot Password page
