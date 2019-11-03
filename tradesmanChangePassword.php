92b1cf6e412a8c99442fe87de47cf8922e39e33f
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-03 20:45:45 +1300

    Added Comments

--- tradesmanChangePassword.php
+++ tradesmanChangePassword.php
@@ -1,15 +1,14 @@
-
+<!-- Page to change Password for Tradesman -->
 
 <?php 
 require_once("heading.php");
-
+//Calling function to update password
 $reset=Tradesman :: changePassword($mysqli, $_POST['email'], $_POST['phone'], $_POST['password']);
- //var_dump($reset);
 if(!$reset){
     $_SESSION['reseterror']="Invalid Email or phone number";
-    header("Location: tradesmanForgotPassword.php");//redirecting to login page
+    header("Location: tradesmanForgotPassword.php");//redirecting to Forgot Password page
 }
- else {
+ else {//On successful updation
    
     $_SESSION['reseterror']="";
    header("Location: tradesmanLogin.php");//redirecting to login page
