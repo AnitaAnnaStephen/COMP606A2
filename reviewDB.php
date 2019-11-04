b1877f0465a69779daeab905718e23c87446f3a7
Gautam <gautam3491@gmail.com>
2019-11-04 15:28:25 +1300

    query update for review

--- reviewDB.php
+++ reviewDB.php
@@ -2,20 +2,20 @@
 <?php
 echo $_POST['jid'];
 require_once("headers.php");
-$comment="";
-if($_POST['rating']==1){
-$comment="Sucks big time";
-}else if($_POST['rating']==2){
-$comment="Kinda bad";
-}else if($_POST['rating']==3){
-$comment="Meh";
-}else if($_POST['rating']==4){
-$comment="Preety good";
-}else if($_POST['rating']==5){
-$comment="Awesome";
-}
+// $comment="";
+// if($_POST['rating']==1){
+// $comment="Sucks big time";
+// }else if($_POST['rating']==2){
+// $comment="Kinda bad";
+// }else if($_POST['rating']==3){
+// $comment="Meh";
+// }else if($_POST['rating']==4){
+// $comment="Preety good";
+// }else if($_POST['rating']==5){
+// $comment="Awesome";
+// }
 //Calling create function in Job class
-$review = Review::create($mysqli,$_POST['jid'], $comment,$_POST['rating']);
+$review = Review::create($mysqli,$_POST['jid'], $_POST['comment'],$_POST['rating']);
 // $today=date('Y-m-d');//getting current date
 if (!($review)){
    
