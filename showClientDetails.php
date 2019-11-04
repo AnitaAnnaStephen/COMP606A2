d61008799207a5633582bf380d98948bae0b37b2
Gautam <gautam3491@gmail.com>
2019-11-04 16:58:54 +1300

    shows comment and star for tradesman

--- showClientDetails.php
+++ showClientDetails.php
@@ -10,6 +10,7 @@ require_once("footer.php");
      <title> Client Details</title>
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
      <style>
+       @import url(//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
 * {
   box-sizing: border-box;
 }
@@ -103,13 +104,44 @@ input[type=submit]:hover {
     margin-top: 0;
   }
 }
-</style>
+fieldset, label { margin: 0; padding: 0; }
+body{ margin: 20px; }
+h1 { font-size: 1.5em; margin: 10px; }
+/****** Style Star Rating Widget *****/
+.rating { 
+  border: none;
+  float: left;
+}
+.rating > input { display: none; } 
+.rating > label:before { 
+  margin-right: 5px;
+  font-size: 1.25em;
+  font-family: FontAwesome;
+  display: inline-block;
+  content: "\f005";
+}
+.rating > .half:before { 
+  content: "\f089";
+  position: absolute;
+}
+.rating > label { 
+  color: #ddd; 
+ float: right; 
+}
+a:hover{
+    text-decoration:none;
+}
+/***** CSS Magic to Highlight Stars on Hover *****/
+.rating > input:checked ~ label, /* show gold star when clicked */
+.rating > input:checked ~ label:hover ~ label { color: #FFED85;  } 
+
+  </style>
 </head>
 
 <body>
 
     
-               <div class="modal-content" style="height: 500px;width: 50%;margin-left: 350px;">
+               <div class="modal-content" style="height: 600px;width: 50%;margin-left: 350px;">
                     <div class="modal-header" style="text-align:center;background-color:#337ab7;">
                          <!-- <button type="button" class="close" data-dismiss="modal">&times;</button>   -->
                          <h4 class="modal-title"><b> Client Details</b></h4>
@@ -121,10 +153,57 @@ input[type=submit]:hover {
                          <div class="row"><div class="col-25"><label>City</label></div> <div class="col-75"><input name="sdate" type="text" disabled value="<?php echo $user->getCity();?>"></div> </div>
                          <div class="row"><div class="col-25"><label>Phone Number</label></div> <div class="col-75"><input name="edate" type="text" disabled value="<?php echo $user->getPhone();?>"></div></div>
                          <div class="row"><div class="col-25"><label>Email </label></div> <div class="col-75"><input name="mcost" type="text" disabled value="<?php echo $user->getEmail();?>"></div></div>
-                         
+                         <?php $r = Review::find($mysqli, $_GET['jid']);
+                         $five="";
+                         $four="";
+                         $three="";
+                         $two="";
+                         $one="";
+                         $rr="";
+                         ?>
+                         <div class="row"><div class="col-25"><label>Comment</label></div> <div class="col-75"><textarea name="comment" disabled type="text" value=""><?php if($r){ echo $r->getComment();}?></textarea></div></div>
+                         <div class="row"><div class="col-25"><label  style="margin-top:13px">Star</label></div> <div class="col-75">
+                         <p>
+                    <!--star-->
+                    <fieldset class="rating">
+                    <?php
+                    if($r){
+                    $rr= $r->getRating();
+                    
+                    if ($rr==5){
+                      $five="checked";
+                    }
+                    if ($rr==4){
+                      $four="checked";
+                    }
+                    if ($rr==3){
+                      $three="checked";
+                    }
+                    if ($rr==2){
+                      $two="checked";
+                    }
+                    if ($rr==1){
+                      $one="checked";
+                    }
+                  }
+                    ?>
+                        <input type="radio" id="star5" name="rating" value="5" <?php echo $five; ?>/><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                        <input type="radio" id="star4" name="rating" value="4" <?php echo $four; ?>/><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                        <input type="radio" id="star3" name="rating" value="3" <?php echo $three; ?>/><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                        <input type="radio" id="star2" name="rating" value="2" <?php echo $two; ?>/><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                        <input type="radio" id="star1" name="rating" value="1" <?php echo $one; ?>/><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+
+
+                    </fieldset>
+
+
+                    <!--start-->
+
+                    </p>   
+                        </div></div>
                          <div class="row"><div class="col-75">
                          <?php $tid= $_SESSION['tid']; ?> 
-                         <div class="link"><?php echo "<a style=\"color:white;\" href=\"TradesmanPage.php?tid=".$tid."\" >Close</a>";?></div>
+                         <div class="link"><?php echo "<a style=\"color:white;\" href=\"viewPastEstimates.php?tid=".$tid."\" >Close</a>";?></div>
                          <!-- <input type="submit" style="text-align:center;" value="Confirm"> -->
                          </div></div>
          
