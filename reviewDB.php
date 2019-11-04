2df89e661baad2b7421e1676fa1a8f2712249276
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-11-04 19:11:42 +1300

    Added comments

--- reviewDB.php
+++ reviewDB.php
@@ -2,19 +2,8 @@
 <?php
 echo $_POST['jid'];
 require_once("headers.php");
-// $comment="";
-// if($_POST['rating']==1){
-// $comment="Sucks big time";
-// }else if($_POST['rating']==2){
-// $comment="Kinda bad";
-// }else if($_POST['rating']==3){
-// $comment="Meh";
-// }else if($_POST['rating']==4){
-// $comment="Preety good";
-// }else if($_POST['rating']==5){
-// $comment="Awesome";
-// }
-//Calling create function in Job class
+
+//Calling create function in review class
 $review = Review::create($mysqli,$_POST['jid'], $_POST['comment'],$_POST['rating']);
 // $today=date('Y-m-d');//getting current date
 if (!($review)){
